5cdb7f98850c6731514d47b10eb0caa99d52a09e
kawhicurry <1145315261@qq.com>
2022-03-08 03:17:49 -0800

    commit

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
-name=
+name=ysy
 
 # 下方内容请不要随意修改
-if [ ! $name ]; then
+if [ $name ]; then
     git add .
     git commit -m 'commit'
-    git push origin $name
+    git push origin master:$name
 else
     echo "Please input your name in push.sh"
 fi
